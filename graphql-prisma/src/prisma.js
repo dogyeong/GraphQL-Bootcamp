e6c84a4ec1ca5bb0a3b24446aa8ef7c35e448c43
dogyeong <shroad1802@naver.com>
2021-04-12 13:47:12 +0900

    Using Async/Await with Prisma Bindings

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -5,4 +5,41 @@ const prisma = new Prisma({
   endpoint: 'http://localhost:4466',
 });
 
+const createPostForUser = async (authorId, data) => {
+  const post = await prisma.mutation.createPost(
+    {
+      data: { ...data, author: { connect: { id: authorId } } },
+    },
+    '{ id }',
+  );
+
+  const user = await prisma.query.user(
+    {
+      where: { id: authorId },
+    },
+    '{ id email name posts { id title published } }',
+  );
+
+  return user;
+};
+
+const updatePostForUser = async (postId, data) => {
+  const post = await prisma.mutation.updatePost(
+    {
+      where: { id: postId },
+      data,
+    },
+    '{ author { id } }',
+  );
+
+  const user = await prisma.query.user(
+    {
+      where: { id: post.author.id },
+    },
+    '{ id email name posts { id title published } }',
+  );
+
+  return user;
+};
+
 export default prisma;
