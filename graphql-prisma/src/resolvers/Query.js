757b8915f5faf03f8b63548e7dc928f41fa47132
dogyeong <shroad1802@naver.com>
2021-04-13 12:35:28 +0900

    Integrating Operation Arguments

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -1,8 +1,24 @@
 const Query = {
   users(parent, args, { prisma }, info) {
+    const opArgs = {};
+
+    if (args.query) {
+      opArgs.where = {
+        OR: [{ name_contains: args.query }, { email_contains: args.query }],
+      };
+    }
+
     return prisma.query.users(null, info);
   },
   posts(parent, args, { prisma }, info) {
+    const opArgs = {};
+
+    if (args.query) {
+      opArgs.where = {
+        OR: [{ title_contains: args.query }, { body_contains: args.query }],
+      };
+    }
+
     return prisma.query.posts(null, info);
   },
   comments(parent, args, { db: { comments } }, info) {
