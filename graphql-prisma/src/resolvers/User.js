48ea4527c1e2a2b9318965c86cbad1f56dc942e5
dogyeong <shroad1802@naver.com>
2021-04-30 21:36:37 +0900

    Locking Down Individual Type Fields

--- graphql-prisma/src/resolvers/User.js
+++ graphql-prisma/src/resolvers/User.js
@@ -1,3 +1,14 @@
-const User = {};
+import { getUserId } from '../utils/getUserId';
+
+const User = {
+  email(parent, args, { req }, info) {
+    const userId = getUserId(req, false);
+
+    if (userId && userId === parent.id) {
+      return parent.email;
+    }
+    return null;
+  },
+};
 
 export default User;
